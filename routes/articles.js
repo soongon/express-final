9e6eeaeae3b9cbe663c6a1874e40568af2545bf1
KITRI-25 <soongon@gmail.com>
2019-05-16 16:25:38 +0900

    mongoose 완료

--- routes/articles.js
+++ routes/articles.js
@@ -1,36 +1,73 @@
 var express = require('express');
 var router = express.Router();
 
-/* /articles 전체보기  */
-router.get('/', (req, res) => {
+const Article = require('../schemas/article');
 
-  res.json(result);
+/* /articles 전체보기  */
+router.get('/', async (req, res, next) => {
+
+  try {
+    const articles = await Article.find({});
+    res.json(articles);
+  } catch (err) {
+    console.log(err);
+    next(err);
+  }
 });
 
 /* /articles/:id 상세보기  */
-router.get('/:articleNum', (req, res) => {
-  const articleNumber = req.params.articleNum;
-  res.send(articleNumber + ' 번글 보기');
+router.get('/:articleNum', async (req, res, next) => {
+  const articleNumber = parseInt(req.params.articleNum);
+
+  try {
+    const article = await Article.find({_id: articleNumber});
+    res.send(article);
+  } catch (err) {
+    console.log(err);
+    next(err);
+  }
 });
 
 /* /articles 등록  */
-router.post('/', (req, res) => {
+router.post('/', async (req, res, next) => {
   const articleJsonFromClient = req.body;
-  res.json(articleJsonFromClient);
+
+  try {
+    const article = new Article(articleJsonFromClient);
+    const saved = await article.save();
+
+    res.json(saved);
+  } catch (err) {
+    console.log(err.message);
+    next(err);
+  }
 });
 
 /* /articles/:id 수정  */
-router.put('/:articleNum', (req, res) => {
-  const articleNumber = req.params.articleNum;
+router.put('/:articleNum', async (req, res, next) => {
+  const articleNumber = parseInt(req.params.articleNum);
   const jsonFromClient = req.body;
 
-  res.send(articleNumber + ' 번 글 수정');
+  try {
+    const modified = await Article.updateOne({_id: articleNumber}, jsonFromClient);
+    res.send(modified);
+  } catch (err) {
+    console.log(err.message);
+    next(err);
+  }
 });
 
 /* /articles/:id 삭제  */
-router.delete('/:articleNum', (req, res) => {
+router.delete('/:articleNum', async (req, res, next) => {
   const articleNumber = parseInt(req.params.articleNum);
-  res.send(articleNumber + ' 번 글 삭제');
+
+  try {
+    const deleted = await Article.deleteOne({_id: articleNumber});
+    res.send(deleted);
+  } catch (err) {
+    console.log(err.message);
+    next(err);
+  }
 });
 
 
